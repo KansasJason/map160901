9487759754b7a653a7c3a77e055b22b684f24632
wangjie <Kansas_Jason@163.com>
2016-09-01 08:19:53 +0800

    提交

---
+++ thinkphp_3.2.3_full/Application/Home/Common/function.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaojiezi
+ * Date: 2016/8/24
+ * Time: 9:49
+ */
+function aa($data,$status,$msg){
+    return array(
+        'data'  => $data,
+        'status' => $status,
+        'msg'   => $msg
+    );
+}
